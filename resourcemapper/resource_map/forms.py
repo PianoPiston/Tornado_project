8eb9fd92947c7ce02a2b268d3ce7cbf52772eb52
CoderTurtl3 <67870996+CoderTurtl3@users.noreply.github.com>
2025-10-04 20:18:10 +0300

    stf

--- resourcemapper/resource_map/forms.py
+++ resourcemapper/resource_map/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CivilianResource
+from .models import CivilianResource, ProfessionalResource
 
 class CivilianResourceForm(forms.ModelForm):
     """
@@ -8,8 +8,9 @@ class CivilianResourceForm(forms.ModelForm):
     """
     class Meta:
         model = CivilianResource
-        fields = ['contact_person', 'resource_type', 'description', 'quantity', 'latitude', 'longitude']
+        fields = ['alias', 'contact_person', 'resource_type', 'description', 'quantity', 'latitude', 'longitude']
         widgets = {
+            'alias': forms.HiddenInput(),
             'description': forms.Textarea(attrs={'rows': 3}),
             'latitude': forms.HiddenInput(),
             'longitude': forms.HiddenInput(),
@@ -20,3 +21,23 @@ class CivilianResourceForm(forms.ModelForm):
         self.fields['contact_person'].widget.attrs.update({'placeholder': 'Contact Info'})
         self.fields['description'].widget.attrs.update({'placeholder': 'e.g., Toyota Hilux, 4 people capacity'})
         self.fields['quantity'].widget.attrs.update({'placeholder': 'e.g., 1 vehicle, 10kg of potatoes'})
+
+
+class ProfessionalResourceForm(forms.ModelForm):
+    """
+    A form for professionals to submit their expertise or service.
+    The location fields are hidden and will be populated by JavaScript on the frontend.
+    """
+    class Meta:
+        model = ProfessionalResource
+        fields = ['alias', 'profession', 'specialty', 'latitude', 'longitude']
+        widgets = {
+            'specialty': forms.TextInput(),
+            'alias': forms.HiddenInput(),
+            'latitude': forms.HiddenInput(),
+            'longitude': forms.HiddenInput(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['specialty'].widget.attrs.update({'placeholder': 'e.g., Cardiologist, Diesel mechanic, Web developer'})
